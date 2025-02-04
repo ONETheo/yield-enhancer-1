10e067353e65a1f80b6b01746b2631fb2780dab9
artemkolodko <temakolodko@gmail.com>
2025-02-04 11:27:17 +0000

    Add boostDAI to metamask

--- client/src/components/YieldBoost.tsx
+++ client/src/components/YieldBoost.tsx
@@ -1,6 +1,8 @@
 import { Header } from './Header';
 import { YieldBoostCard } from './YieldBoostCard';
 import { useYieldBoost } from '@/hooks/useYieldBoost';
+import {useAccount} from "wagmi";
+import {appConfig} from "@/config.ts";
 
 export default function YieldBoost() {
   const {
@@ -17,6 +19,38 @@ export default function YieldBoost() {
     inProgress,
     activeTab
   } = useYieldBoost();
+
+  const {connector} = useAccount();
+
+  const onAddClick = async () => {
+    try {
+      if(!connector) {
+        return
+      }
+      const provider = await connector.getProvider() as any
+      const wasAdded = await provider
+        .request({
+          method: "wallet_watchAsset",
+          params: {
+            type: "ERC20",
+            options: {
+              address: appConfig.stakingVaultAddress,
+              symbol: 'boostDAI',
+              decimals: 18,
+              image: "",
+            },
+          },
+        })
+
+      if (wasAdded) {
+        console.log("Token was added")
+      } else {
+        console.log("Token wasn't added to Metamask")
+      }
+    } catch (e) {
+      console.error('Failed to add token to Metamask:', e);
+    }
+  }
 
   return (
     <div className="min-h-screen bg-[#111111] text-white">
@@ -46,6 +80,18 @@ export default function YieldBoost() {
             *0.1% fee for deposits and withdrawals
           </p>
         </div>
+        {connector &&
+            <div
+                onClick={onAddClick}
+                style={{
+                  cursor: 'pointer',
+                  marginTop: '2px',
+                  borderBottom: '1px solid #dddddd'
+            }}
+            >
+                Add boostDAI to MetaMask
+            </div>
+        }
       </div>
     </div>
   );
